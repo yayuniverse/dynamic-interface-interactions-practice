497b70150229669bbbebacd11d807834576a3322
Oye <66223246+yayuniverse@users.noreply.github.com>
2025-05-29 22:50:53 +0100

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default defineConfig([
+  { ignores: ["eslint.config.mjs"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
+  ...compat.extends("airbnb-base"),
+  eslintConfigPrettier,
+]);
