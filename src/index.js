7625d7add582a03d53b32281085beea5aecbfaf1
yay <yaytrxsh@proton.me>
2025-05-31 14:53:28 +0100

    fix: add missing semicolons in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "./reset.css"
-import "./global.css"
-import "./dropdown.css"
-import makeDropdown from "./dropdown"
+import "./reset.css";
+import "./global.css";
+import "./dropdown.css";
+import makeDropdown from "./dropdown";
 
-makeDropdown(document.querySelector(".dropdown-wrapper"))
+makeDropdown(document.querySelector(".dropdown-wrapper"));
