88486245b7b4209761e6125395edfde59377bb23
yay <yaytrxsh@proton.me>
2025-05-31 22:38:58 +0100

    feat: add carousel component styling and structure

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import "./reset.css";
 import "./global.css";
 import "./dropdown.css";
 import makeDropdown from "./dropdown";
+import "./carousel.css";
 
 makeDropdown(document.querySelector(".dropdown-wrapper"));
